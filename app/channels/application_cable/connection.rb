8f1a49d202560ad99a9193f74dba7fe8c25fc3be
John Lawrence <johnonrails@gmail.com>
2017-01-10 15:35:24 -0800

    Add authentication step in Connection (commented out)

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    # identified_by :current_user
+    #
+    # def connect
+    #   self.current_user = find_verified_user
+    # end
+    #
+    # private
+    #
+    # def find_verified_user
+    #   cookie = cookies.signed['user.id']
+    #   if current_user = User.find_by(id: cookie)
+    #     current_user
+    #   else
+    #     reject_unauthorized_connection
+    #   end
+    # end
   end
 end
